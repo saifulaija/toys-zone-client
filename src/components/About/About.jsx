0cb837aa4e7c484dd7b9db8e05b692985076aa58
saifulaija <saifulaija2023@gmail.com>
2023-05-21 04:35:50 +0600

    banner section modified

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,28 +1,50 @@
-import React from 'react';
-import ceo from '../../../public/ceo.jpg'
-import subceo from '../../../public/subceo.jpg'
+import React from "react";
+import ceo from "../../../public/ceo.jpg";
+import subceo from "../../../public/subceo.jpg";
 import { CiViewColumn } from "react-icons/ci";
 
 const About = () => {
-      return (
-            <div className='w-full p-32 '>
-                  <h1 className='heading my-12'>About Us</h1>
+  return (
+    <div className="w-full p-32 ">
+      <h1 className="heading my-12">About Us</h1>
 
-                  <div className=' my_container md:flex justify-between items-center'>
-                        <div className='md:w-1/2 relative' data-aos="zoom-out-up" data-aos-time="2000">
-                              <img className='rounded-lg' src={ceo} alt="" />
-                              <img className='absolute ml-36 rounded-lg -mt-32 shadow-2xl' src={subceo} alt="" />
-                        </div>
-                        <div className='md:w-1/2 space-y-8' data-aos="zoom-out-up" data-aos-time="2000">
-                              <h1 className='text-3xl font-bold uppercase tracking-widest underline-offset-2'>Our Mission</h1>
-                              <p>My mission as an AI language model is to assist and provide helpful and accurate information to users like you. I strive to answer questions, engage in conversation, and assist with various topics to the best of my abilities. If you have any specific questions or need assistance with a particular subject, feel free to ask!</p>
-                              <button className='primary-button bg-yellow-700 text-white'>Explore More <CiViewColumn className='inline-block'></CiViewColumn> </button>
-
-                        </div>
-                  </div>
-                  
-            </div>
-      );
+      <div className=" my_container md:flex justify-between items-center">
+        <div
+          className="md:w-1/2 relative"
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+        >
+          <img className="rounded-lg" src={ceo} alt="" />
+          <img
+            className="absolute ml-36 rounded-lg -mt-32 shadow-2xl"
+            src={subceo}
+            alt=""
+          />
+        </div>
+        <div
+          className="md:w-1/2 space-y-8"
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+        >
+          <h1 className="text-3xl font-bold uppercase tracking-widest underline-offset-2">
+            Our Mission
+          </h1>
+          <p>
+            My mission as an AI language model is to assist and provide helpful
+            and accurate information to users like you. I strive to answer
+            questions, engage in conversation, and assist with various topics to
+            the best of my abilities. If you have any specific questions or need
+            assistance with a particular subject, feel free to ask!
+          </p>
+          <button className="primary-button bg-yellow-700 text-white">
+            Explore More <CiViewColumn className="inline-block"></CiViewColumn>{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
-import image1 from '../../../public/banner-1.jpg'
-import image2 from '../../../public/banner-2.jpg'
-import {AiOutlineShoppingCart } from "react-icons/ai";
-import ReactTyped from 'react-typed';
+import React from "react";
 
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
+import "./Banner.css";
+import banner from "../../../public/banner.jpg";
 
 const Banner = () => {
-      return (
-            <div>
-
-<div className="hero min-h-screen" style={{ backgroundImage: `url("../../../public/banner-1.jpg")` }}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-lg leading-6">
-      <h1 className="mb-5 text-2xl tracking-wider font-sans text-white md:text-4xl uppercase font-bold">well come To <span className='text-yellow-800'>Toys Zone</span> for your baby</h1>
-      <p className='text-3xl font-semibold text-red-950 space-y-4'>Toys Zone Sells </p>
-      <ReactTyped className='text-teal-900 font-extrabold tracking-widest text-2xl' strings={['POLICE TOY, TRUCK, REACHING CAR', 'FANTASTIC TOYS']} typeSpeed={180} backDelay={150} loop></ReactTyped>
-      <p className="mb-5">Toys are objects designed for play and amusement, often intended for children. They come in various forms, such as action figures, dolls, board games, puzzles, building blocks, stuffed animals, vehicles, and electronic.</p>
-      <button className=" bg-[#008eaa]  px-6 py-2  rounded-lg animate-bounce uppercase tracking-widest">Get Started <AiOutlineShoppingCart className='ml-2 inline-block'></AiOutlineShoppingCart> </button>
-    </div>
-  </div>
-</div>
-                  
-            </div>
-      );
+  return (
+    <div className="w-full banner">
+      <div className="my_container gap-6  p-40 md:flex justify-between items-center ">
+        <div
+          className="md:w-1/2 leading-6 space-y-8 "
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+        >
+          <h1 className="text-2xl md:text-4xl font-bold uppercase text-white tracking-widest">
+            well come to our shop
+          </h1>
+          <p className="mb-5 text-white">
+            Toys are objects designed for play and amusement, often intended for
+            children. They come in various forms, such as action figures, dolls,
+            board games, puzzles, building blocks, stuffed animals, vehicles,
+            and electronic.
+          </p>
+          <button className=" bg-[#008eaa]  px-6 py-2  rounded-lg animate-bounce uppercase tracking-widest">
+            Get Started{" "}
+            <AiOutlineShoppingCart className="ml-2 inline-block"></AiOutlineShoppingCart>{" "}
+          </button>
+        </div>
+        <div
+          className="md:w-1/2"
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+        >
+          <img className="rounded-2xl shadow-2xl" src={banner} alt="" />
+        </div>
+      </div>
+     </div>
+  );
 };
 
 export default Banner;
+
+
